fe686138271be09960355538bf75a2d3cd64ba6f
Tiago <tiago.rainho@ua.pt>
2022-11-21 13:27:06 +0000

    change ants path to colony to make it more organic, euclidean method used instead of manhattan

--- src/states/ant_fsm.py
+++ src/states/ant_fsm.py
@@ -3,6 +3,7 @@ from states.fsm import FSM, State, Transition
 from random import randint
 
 from models.pheromone import Pheromone
+from math import dist
 
 class AntFSM(FSM):
 
@@ -54,29 +55,41 @@ class ReturnColony(State):
 
         # compute vector to colony
         vector = [ant.colony.x - ant.x, ant.colony.y - ant.y]
-        x_abs, y_abs = abs(vector[0]), abs(vector[1])
 
-        normalized_vector = [1, 1]
-        if x_abs > y_abs:
-            normalized_vector[1] = 0
-        if x_abs < y_abs:
-            normalized_vector[0] = 0
-            
-        # fix vector direction
-        if normalized_vector[0] != 0:
-            normalized_vector[0] *= 1 if vector[0] > 0 else -1
+        delta_x = 0
+        delta_y = 0
 
-        if normalized_vector[1] != 0:
-            normalized_vector[1] *= 1 if vector[1] > 0 else -1
+        # normalize delta x and delta y
+        if vector[0] != 0:
+            delta_x = 1 if vector[0] > 0 else -1
 
+        if vector[1] != 0:
+            delta_y = 1 if vector[1] > 0 else -1
+
+        adjacent_coordinates = [
+            (ant.x + delta_x, ant.y),
+            (ant.x + delta_x, ant.y + delta_y),
+            (ant.x, ant.y + delta_y)
+        ]
+
+        # get colony vector normalized
+        colony_vector_length = dist((ant.colony.x, ant.colony.y), (ant.x, ant.y))
+        colony_vector_normalized = (vector[0]/colony_vector_length, vector[1]/colony_vector_length)
+        normalized_to_colony_vector = (ant.x + colony_vector_normalized[0], ant.y + colony_vector_normalized[1])
+
+        # compute closest coordinate to the colony vector normalized
+        closest_coordinate = min(adjacent_coordinates, key=lambda possible_closest_coord: dist(normalized_to_colony_vector, possible_closest_coord))
+
+        # convert the coordinate to move to, to a vector for the ant to move
+        normalized_vector = (closest_coordinate[0] - ant.x, closest_coordinate[1] - ant.y)
 
         # release pheromones while returning home
         ant.release_pheromone(amount = Pheromone.DEFAULT_INCREASE_AMOUNT/ant.steps_from_food)
         
         # pheromone release alternative functions
+
         # ant.release_pheromone(amount = Pheromone.DEFAULT_INCREASE_AMOUNT)
         # ant.release_pheromone(amount = pow(10*ant.steps_from_food, -2) + Pheromone.DEFAULT_INCREASE_AMOUNT)
         # ant.release_pheromone(amount = Pheromone.MAXIMUM_AMOUNT-Pheromone.MAXIMUM_AMOUNT * (ant.steps_from_food/Pheromone.MAXIMUM_STEPS))
 
         ant.walk(vector = normalized_vector)
-        
